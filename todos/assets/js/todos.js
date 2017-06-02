4618f5b650414066392be165f38c063ff26dceb1
Andrew James <ajames20@gmail.com>
2017-06-02 15:20:55 +0300

    todo app complete

--- todos/assets/js/todos.js
+++ todos/assets/js/todos.js
@@ -1,10 +1,24 @@
-$('li').click(function() {
+$('ul').on('click', 'li', function() {
   $(this).toggleClass('completed');
 });
 
-$('span').click(function(e) {
+$('ul').on('click', 'span', function(e) {
   $(this).parent().fadeOut(500, function() {
     $(this).remove();
   });
   e.stopPropagation();
+});
+
+$('input[type = "text"]').keypress(function(e) {
+  if (e.which === 13) {
+    let todoText = $(this).val();
+    $(this).val('');
+    $('ul').append(
+      `<li><span><i class="fa fa-trash"></i></span> ${todoText}</li>`
+    );
+  }
+});
+
+$('.fa-plus').click(function() {
+  $('input[type="text"]').fadeToggle();
 });
