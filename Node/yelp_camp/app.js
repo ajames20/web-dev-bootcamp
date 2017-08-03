c0e09eb3da822d930ad5ad27a0a9bb3ee3dc408a
Andrew James <ajames20@gmail.com>
2017-08-03 08:32:32 +0300

    add form to add new campground

--- Node/yelp_camp/app.js
+++ Node/yelp_camp/app.js
@@ -5,22 +5,27 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
+const campgrounds = [
+  { name: 'Salmon Creek', image: 'https://farm5.staticflickr.com/4153/4835814837_feef6f969b.jpg' },
+  { name: 'Goats Breath', image: 'https://farm4.staticflickr.com/3270/2617191414_c5d8a25a94.jpg' },
+  { name: 'Bears Water', image: 'https://farm3.staticflickr.com/2259/2182093741_164dc44a24.jpg' }
+];
+
 app.get('/', (req, res) => {
   res.render('landing');
 });
 
 app.get('/campgrounds', (req, res) => {
-  const campgrounds = [
-    { name: 'Salmon Creek', image: 'https://farm5.staticflickr.com/4153/4835814837_feef6f969b.jpg' },
-    { name: 'Goats Breath', image: 'https://farm4.staticflickr.com/3270/2617191414_c5d8a25a94.jpg' },
-    { name: 'Bears Water', image: 'https://farm3.staticflickr.com/2259/2182093741_164dc44a24.jpg' }
-  ]
   res.render('campgrounds', { campgrounds: campgrounds })
 });
 
 app.post('/campgrounds', (req, res) => {
-  res.send('You hit the post route.');
-  // console.log(JSON.parse(req.body.name))
+  let name = req.body.name;
+  let image = req.body.image;
+  let newCampground = { name: name, image: image };
+  campgrounds.push(newCampground);
+  // redirect to campgrounds page
+  res.redirect('/campgrounds');
 });
 
 app.get('/campgrounds/new', (req, res) => {
