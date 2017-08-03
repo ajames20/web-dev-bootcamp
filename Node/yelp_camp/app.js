2767146612da84c7b0ec28beb322704066f3af76
Andrew James <ajames20@gmail.com>
2017-08-03 07:19:56 +0300

    add yelpCamp basic structure

---
+++ Node/yelp_camp/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+// temp array for building campgrounds
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+  res.render('landing');
+});
+
+app.get('/campgrounds', (req, res) => {
+  const campgrounds = [
+    { name: 'Salmon Creek', image: 'https://farm5.staticflickr.com/4153/4835814837_feef6f969b.jpg' },
+    { name: 'Goats Breath', image: 'https://farm4.staticflickr.com/3270/2617191414_c5d8a25a94.jpg' },
+    { name: 'Bears Water', image: 'https://farm3.staticflickr.com/2259/2182093741_164dc44a24.jpg' }
+  ]
+  res.render('campgrounds', { campgrounds: campgrounds })
+});
+
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
