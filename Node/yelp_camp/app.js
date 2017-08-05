e0f13eaaa986f489638031961f2a6560010c298e
Andrew James <ajames20@gmail.com>
2017-08-06 00:00:05 +0300

    add data persitance

--- Node/yelp_camp/app.js
+++ Node/yelp_camp/app.js
@@ -11,30 +11,49 @@ app.set('view engine', 'ejs');
 // SCHEMA SETUP
 const campgroundsSchema = new mongoose.Schema({
   name: String,
-  image: String
+  image: String,
+  description: String
 });
 
 const Campground = mongoose.model('Campground', campgroundsSchema);
+
+// Campground.create(
+//   {
+//     name: 'Lake Montain',
+//     image: 'https://farm3.staticflickr.com/2923/13950231147_7032e443a0.jpg',
+//     description: 'This is a lake next to a mountain!!'
+//   }, (err, campground) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       // redirect to campgrounds page
+//       console.log('We made a new Campground with a description')
+//       console.log(campground)
+//     }
+//   });
 
 app.get('/', (req, res) => {
   res.render('landing');
 });
 
+// INDEX ROUTE - show all campgrounds
 app.get('/campgrounds', (req, res) => {
   // Get all campgrounds from db and then render campgrounds
   Campground.find({}, (err, allCampgrounds) => {
     if (err) {
       console.log(err);
     } else {
-      res.render('campgrounds', { campgrounds: allCampgrounds });
+      res.render('index', { campgrounds: allCampgrounds });
     };
   });
 });
 
+// CREATE ROUTE - add new campground to DB
 app.post('/campgrounds', (req, res) => {
   var name = req.body.name;
   var image = req.body.image;
-  var newCampground = { name: name, image: image };
+  var desc = req.body.description;
+  var newCampground = { name: name, image: image, description: desc };
   // Create a new Campground and save to DB
   Campground.create(newCampground, (err, newlyCreated) => {
     if (err) {
@@ -46,8 +65,22 @@ app.post('/campgrounds', (req, res) => {
   });
 });
 
+// NEW ROUTE - show form to create new campground
 app.get('/campgrounds/new', (req, res) => {
   res.render('new.ejs');
+});
+
+// SHOW ROUTE - shows more info about individual campground
+app.get('/campgrounds/:id', (req, res) => {
+  // Find campground by ID
+  Campground.findById(req.params.id, (err, foundCampground) => {
+    if (err) {
+      console.log(err)
+    } else {
+      // Render that individual campground
+      res.render('show', { campground: foundCampground })
+    }
+  });
 });
 
 app.listen(3000, () => {
