b61b020807458af61c2054439b954af3d1097875
Andrew James <ajames20@gmail.com>
2017-08-07 11:02:36 +0300

    add semantic styles and form for new posts

--- Node/RESTblog/app.js
+++ Node/RESTblog/app.js
@@ -30,12 +30,31 @@ app.get('/', (req, res) => {
   res.redirect('/blogs');
 });
 
+// INDEX ROUTE
 app.get('/blogs', (req, res) => {
   Blog.find({}, (err, blogs) => {
     if (err) {
       console.log(err);
     } else {
       res.render('index', { blogs: blogs });
+    }
+  });
+});
+
+// NEW ROUTE
+app.get('/blogs/new', (req, res) => {
+  res.render('new');
+});
+
+// CREATE ROUTE
+app.post('/blogs', (req, res) => {
+  // create blog
+  Blog.create(req.body.blog, (err, newBlog) => {
+    if (err) {
+      res.render('new');
+    } else {
+      // redirect to blogs
+      res.redirect('/blogs');
     }
   });
 });
