8b35b69e843c7fdd402152130fe1fe543bec9934
Andrew James <ajames20@gmail.com>
2017-08-09 12:49:53 +0300

    add destroy route

--- Node/yelp_camp/routes/comments.js
+++ Node/yelp_camp/routes/comments.js
@@ -66,7 +66,17 @@ router.put('/:comment_id', (req, res) => {
       res.redirect(`/campgrounds/${req.params.id}`);
     }
   });
+});
 
+// COMMENT DESTROY ROUTE
+router.delete('/:comment_id', (req, res) => {
+  Comment.findByIdAndRemove(req.params.comment_id, (err) => {
+    if (err) {
+      res.redirect('back');
+    } else {
+      res.redirect(`/campgrounds/${req.params.id}`);
+    }
+  });
 });
 
 // MIDDLEWARE isLoggedIn
