8045d48b2068ed82f2d39fabdd16fec35e3e50eb
Andrew James <ajames20@gmail.com>
2017-08-15 16:48:28 +0300

    update to loaders for yelp camp

--- Node/yelp_camp/public/scripts/search.js
+++ Node/yelp_camp/public/scripts/search.js
@@ -1,30 +1,43 @@
-$('#campground-search').on('input', function () {
-  var search = $(this).serialize();
+$(window).on('load', () => {
+  // main image loaded ?
+  $('.thumbnail-image').imagesLoaded({ background: true }).
+    always(() => {
+      Array.from($('.thumbnail-image')).forEach((div) => {
+        $('.thumbnail-image').fadeIn();
+      });
+    }).done(() => {
+      $('.loader').remove();
+    });
 
-  if (search === "search=") {
-    search = "all";
-  }
-  $.get('/campgrounds?' + search, (data) => {
-    $('#campground-grid').html('');
-    data.forEach((campground) => {
-      $('#campground-grid').append(`
-        <div class="col-md-3 col-sm-6">
-          <div class="thumbnail">
-            <img class="max-image" src="${campground.image}">
-            <div class="caption">
-              <h4>${campground.name}</h4>
+
+  // timeout to remove flash message from the DOM
+  setTimeout(() => {
+    $(".message").hide();
+  }, 3000);
+
+  $('#campground-search').on('input', function () {
+    var search = $(this).serialize();
+
+    if (search === "search=") {
+      search = "all";
+    }
+    $.get('/campgrounds?' + search, (data) => {
+      $('#campground-grid').html('');
+      data.forEach((campground) => {
+        $('#campground-grid').append(`
+          <div class="col-xs-12 col-sm-6 col-md-4">
+            <div class="thumbnail">
+            <div class="thumbnail-image" style="background: url('${campground.image}') no-repeat center; background-size: cover; max-height: 250px;"></div>
+              <div class="caption">
+                <h4>${campground.name}</h4>
+              </div>
+              <p>
+                <a href="/campgrounds/${campground._id}" class="btn btn-primary">More Info</a>
+              </p>
             </div>
-            <p>
-              <a href="/campgrounds/${campground._id}" class="btn btn-primary">More Info</a>
-            </p>
           </div>
-        </div>
-      `);
+        `);
+      });
     });
   });
 });
-
-// timeout to remove flash message from the DOM
-setTimeout(() => {
-  $(".message").hide();
-}, 2550);
