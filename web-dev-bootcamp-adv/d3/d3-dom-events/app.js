6f62f910357e5e047e9f8afffdaa19c7874647f5
Andrew James <ajames20@gmail.com>
2017-10-25 02:49:45 +0300

    update note app with d3

--- web-dev-bootcamp-adv/d3/d3-dom-events/app.js
+++ web-dev-bootcamp-adv/d3/d3-dom-events/app.js
@@ -1,11 +1,36 @@
+const input = d3.select('input');
+const preview = d3.select('.preview');
+
+const setValue = val => {
+  preview.text(val).classed('hide', val === '');
+};
+
+d3.select('.remove').on('click', () => {
+  d3.selectAll('.note').remove();
+});
+
+d3.select('.lucky').on('click', () => {
+  d3.selectAll('.note').style('font-size', () => {
+    return Math.random() * 100 + 'px';
+  });
+});
+
 d3.select('#new-note').on('submit', () => {
   d3.event.preventDefault();
 
-  const input = d3.select('input');
+  if (!input.property('value')) return;
+
   d3
     .select('#notes')
     .append('p')
     .classed('note', true)
     .text(input.property('value'));
+
   input.property('value', '');
+  setValue('');
+});
+
+input.on('input', () => {
+  const note = d3.event.target.value;
+  setValue(note);
 });
