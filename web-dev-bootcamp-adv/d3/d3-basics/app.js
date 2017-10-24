d08cffda36987ac64056ec7ef395257394b607dc
Andrew James <ajames20@gmail.com>
2017-10-24 15:14:22 +0300

    add basic d3 event listeners

---
+++ web-dev-bootcamp-adv/d3/d3-basics/app.js
@@ -0,0 +1,8 @@
+d3
+  .select('.outer')
+  .style('color', 'purple')
+  .select('div')
+  .style('font-size', '30px')
+  .style('background-color', 'orange')
+  .select('div')
+  .style('border', '8px solid blue');
